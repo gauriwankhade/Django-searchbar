cefc343d8cb14886b16cb6e0de54bd6f68dc5ffd
gauriwankhade <gauriwankhade888@gmail.com>
2021-06-17 10:31:02 +0530

    added whitenoise

--- mymaps/settings.py
+++ mymaps/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #added by me
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static") ]
+#STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static") ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
